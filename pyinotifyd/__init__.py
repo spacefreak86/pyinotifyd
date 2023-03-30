56b19120f4283830d09748adc74661e5638f3811
Thomas Oettli <spacefreak@noop.ch>
2023-03-30 14:10:56 +0200

    fix version

--- pyinotifyd/__init__.py
+++ pyinotifyd/__init__.py
@@ -36,7 +36,7 @@ from pyinotify import ProcessEvent, ExcludeFilter
 from pyinotifyd._install import install, uninstall
 from pyinotifyd.scheduler import TaskScheduler, Cancel
 
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 
 
 def setLoglevel(loglevel, logname=None):
